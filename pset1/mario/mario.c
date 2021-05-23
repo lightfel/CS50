7700d3d7bf71bbfe697aee8537200a98abdd7ca8
lightfel <motomoto.long2222@gmail.com>
2021-05-23 13:24:46 +0000

    increase for

--- pset1/mario/mario.c
+++ pset1/mario/mario.c
@@ -9,13 +9,10 @@ int main(void)
     } while (height < 1 || height > 8);
     for (int i = 0; i < height; i++)
     {
-        for (int j = height - i - 1; j > 0; j--)
-            write(1, " ", 1);
-        for (int k = 0; k <= i; k++)
-            write(1, "#", 1);
+        write(1, "       ", height - i - 1);
+        write(1, "########", i + 1);
         write(1, "  ", 2);
-        for (int l = 0; l <= i; l++)
-            write(1, "#", 1);
+        write(1, "########", i + 1);
         write(1, "\n", 1);
     }
 }
